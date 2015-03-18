ee5ca9e4d5f89aa55a68e30358c115621b4ff04c
zhanglin9833 <zhanglin9833@gmail.com>
2015-03-18 17:55:27 +0800

    add ibe.c

---
+++ ibe-plugin/src/ibe.c
@@ -0,0 +1,93 @@
+/*
+ * Program: Identity-Based Encryption - Plugin for Claws Mail
+ * Author: Zhang Lin <zhanglin9833@gmail.com>
+ * Depends: Claws Mail, GMP, PBC 
+ */
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include <glib.h>
+#include <glib/gi18n.h>
+
+#include "defs.h"
+#include "version.h"
+#include "claws.h"
+#include "plugin.h"
+#include "utils.h"
+#include "hooks.h"
+#include "pop.h"
+
+#define PLUGIN_NAME (_("Identity-Based Encryption"))
+
+gboolean ibe_decrypt_hook(gpointer source, gpointer data)
+{
+    MailReceiveData *mail_receive_data = (MailReceiveData *)source;
+
+    gchar *mail_data = mail_receive_data->data;
+    g_warning("$$$ %s\n", mail_data);
+	return FALSE;
+}
+
+static guint ibe_decrypt_hook_id;
+
+gint plugin_init(gchar **error)
+{
+	if (!check_plugin_version(MAKE_NUMERIC_VERSION(2,9,2,72),
+				VERSION_NUMERIC, PLUGIN_NAME, error))
+		return -1;
+
+	ibe_decrypt_hook_id = hooks_register_hook(MAIL_RECEIVE_HOOKLIST, ibe_decrypt_hook, NULL);
+	if (ibe_decrypt_hook_id == -1) {
+		*error = g_strdup(_("Failed to register ibe decrypt hook"));
+		return -1;
+	}
+
+	g_print("Identity-Based Encryption plugin loaded\n");
+
+	return 0;
+}
+
+gboolean plugin_done(void)
+{
+	hooks_unregister_hook(MAIL_RECEIVE_HOOKLIST, ibe_decrypt_hook_id);
+
+	g_print("Identity-Based Encryption plugin unloaded\n");
+	return TRUE;
+}
+
+const gchar *plugin_name(void)
+{
+	return PLUGIN_NAME;
+}
+
+const gchar *plugin_desc(void)
+{
+	return _("This Plugin encrypt/decrypt the email using IBE, "
+	         "Identity-Based Encryption from the Weil Pairing."
+	         );
+}
+
+const gchar *plugin_type(void)
+{
+	return "Common";
+}
+
+const gchar *plugin_licence(void)
+{
+	return "GPL3+";
+}
+
+const gchar *plugin_version(void)
+{
+	return VERSION;
+}
+
+struct PluginFeature *plugin_provides(void)
+{
+	static struct PluginFeature features[] = 
+		{ {PLUGIN_OTHER, N_("Identity-Based Encryption")},
+		  {PLUGIN_NOTHING, NULL}};
+	return features;
+}
